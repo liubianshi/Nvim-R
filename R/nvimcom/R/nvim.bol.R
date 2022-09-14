2dc6426f0771b1414a9f797bd0cca77fa2bdbe2b
liu.bian.shi <liu.bian.shi@gmail.com>
2022-09-14 10:55:54 +0800

    解除警报: in coercion to logical(1)

--- R/nvimcom/R/nvim.bol.R
+++ R/nvimcom/R/nvim.bol.R
@@ -124,7 +124,7 @@ nvim.args <- function(funcname, txt = "", pkg = NULL, objclass, extrainfo = FALS
         res <- paste0(res, collapse='')
 
 
-    if(length(res) == 0 || res == ""){
+    if(length(res) == 0 || all(res == "")){
         res <- "[]"
     } else {
         if(is.null(pkg)){
